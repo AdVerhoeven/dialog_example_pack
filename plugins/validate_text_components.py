7b45b41729554374a2bec508886e71e2a5430489
AdVerhoeven <61581342+AdVerhoeven@users.noreply.github.com>
2026-01-05 10:19:16 +0100

    Added alt language checking

--- plugins/validate_text_components.py
+++ plugins/validate_text_components.py
@@ -5,6 +5,7 @@ import mecha
 
 mc = Mecha()
 
+
 def check_dialogs(ctx: Context):
     for (name, dialog) in ctx.data.dialogs.items():
         for (key, value) in dialog.data.items():
@@ -16,11 +17,14 @@ def check_dialogs(ctx: Context):
                     validate_inner_json_text(value)
         break
 
+
 def validate_json_text(text_component: dict):
     print(text_component)
 
+
 def validate_inner_json_text(jobject: dict):
     print("do something")
+
 
 def check_functions(ctx: Context):
     en_us = collect_default_lang(ctx, "en_us")
@@ -34,8 +38,8 @@ def check_functions(ctx: Context):
                     case mecha.AstNbtCompoundEntry():
                         if x.key.value == "text":
                             # check if text is not empty, not a whitespace helper or a placeholder (starting with @)
-                            if not (len(x.value.value) == 0 
-                                    or x.value.value.isspace() 
+                            if not (len(x.value.value) == 0
+                                    or x.value.value.isspace()
                                     or x.value.value.startswith("@")):
                                 printnode(x, name)
                         if x.key.value == "translate":
@@ -46,20 +50,44 @@ def check_functions(ctx: Context):
                                 print("empty fallback")
     print("Done checking functions")
 
+
 def printnode(node: AstNbtCompoundEntry, name):
     print(name + " | " + node.key.location.__str__())
     print(node.value.value)
     print()
 
-def collect_default_lang(ctx: Context, language: str) -> dict | None:
+
+def collect_default_lang(ctx: Context, lang_code: str = "en_us") -> dict | None:
     language_dict = dict()
+    print(f"Scanning for default language code '{lang_code}'")
     for (key, lang) in ctx.assets.languages.items():
-        if key.endswith(language):
+        if key.endswith(lang_code):
             language_dict.update(lang.data)
             print(f"Added translation keys of file: '{key}'")
     print()
-    print(f"Done collecting translation keys for language code '{language}'")
+    print(f"Done collecting translation keys for language code '{lang_code}'")
     print()
-    # print(language_dict)
 
     return language_dict
+
+
+def validate_alt_languages(ctx: Context, lang_code: str = "en_us"):
+    default_lang = collect_default_lang(ctx, lang_code)
+    alt_langs = dict()
+    for (key, lang) in ctx.assets.languages.items():
+        if not key.endswith(lang_code):
+            alt_code = key[-5:]
+            print(
+                f"Found '{key}', adding/merging into collector for '{alt_code}'")
+            if not alt_code in alt_langs:
+                alt_langs[alt_code] = lang.data
+            else:
+                alt_langs[alt_code].update(lang.data)
+
+    print("Scanning for missing translation keys")
+    for (lang_code, alt_language) in alt_langs.items():
+        print(f"Scanning: '{lang_code}'")
+        for tk in default_lang.keys():
+            if not tk in alt_language:
+                print(f"Missing: \"{tk}\"")
+        print(f"Done scanning '{lang_code}'\n")
